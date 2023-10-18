17d2752b8bd8119b893f0ac99630d4bedd4695fb
ahmed osman <ahmedos099912@gmail.com>
2023-10-18 19:38:06 +0200

    textfiled for input

--- src/Home.js
+++ src/Home.js
@@ -5,12 +5,15 @@ import { add,remove } from './redux/blogs/Actions'
 function Home(props) {
     console.log(props.blogs)
     const [inputValue,setInputValue] =  useState('aaa')
-
+    const [inputValue2,setInputValue2] =  useState('aaa')
     const handleInputChange = (event)=>{
         setInputValue(event.target.value)
     }
+    const handleInputChange2 = (event)=>{
+        setInputValue2(event.target.value)
+    }
     const additemtostate= (inputValue)=>{
-        props.add({id:3,title:inputValue,body:'second best web dev tool'})
+        props.add({id:3,title:inputValue,body:inputValue2})
     }
     // const handleClick = ()=>{
     //     setCounter(counter+1)
@@ -37,6 +40,11 @@ function Home(props) {
           type="text"
           value={inputValue}
           onChange={handleInputChange}
+        />
+        <input
+          type="text"
+          value={inputValue2}
+          onChange={handleInputChange2}
         />
         <button onClick={()=>additemtostate(inputValue)}>add to List</button>
         <button onClick={()=>props.remove(1)}>decrease</button>
